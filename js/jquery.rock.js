d54371526bcc69218211f215f89974740df5f83e
Christian Haller <christianhaller@christian.office.visions>
2010-12-07 23:06:35 +0100

    last man standing

--- js/jquery.rock.js
+++ js/jquery.rock.js
@@ -209,8 +209,7 @@
                         window.clearTimeout(this);
                     });
                     var id = window.setTimeout(function(){
-                        enter = '';
-                        $rock.$last = null;
+                       enter = '';
                     }, settings.timeout);
 
                     timeout.push(id);
@@ -230,11 +229,7 @@
 
                         }
                         // nothing found
-                        if(index===$rock.buttons.length-1){
-                           if($rock.last){
-                               $rockl.$last.hover().focus();
-                           }
-                        }
+
                     });
 			// Wirft rockdown hinter origin dropdown
 			}).delegate('li.' + settings.optionClass, 'mouseover', function(){
